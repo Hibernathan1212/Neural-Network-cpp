6efd7e011dc7a77d645a8cb21ae040e35869f606
Nathan <nnthurber@icloud.com>
2024-06-28 17:13:59 +0700

    Initial Commit

---
+++ Neural network/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Neural network
+//
+//  Created by Nathan Thurber on 28/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
